7fefe71d43d940d027d951552ef030b3ae666132
Altitech-online <spalmer.developments@gmail.com>
2022-05-16 13:49:54 +0100

    added actions

---
+++ cucumber-html-report.js
@@ -0,0 +1,19 @@
+const report = require('multiple-cucumber-html-reporter');
+
+report.generate({
+	jsonDir: 'cypress/cucumber-json',
+	reportPath: './test-report/cucumber-report',
+    reportSuiteAsScenarios: true,
+    launchReport: true,
+	metadata:{
+        browser: {
+            name: 'chrome',
+            version: '98.0.4758.102'
+        },
+        device: 'Local test machine',
+        platform: {
+            name: 'Windows',
+            version: '11'
+        }
+    }
+});
